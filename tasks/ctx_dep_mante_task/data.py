bcd5af3d460602b967395abb24ecde3e01f78422
Alperen Cimen <alperen.cimen@ozu.edu.tr>
2025-03-18 14:42:28 +0300

    second version on ctx-dep task

--- tasks/ctx_dep_mante_task/data.py
+++ tasks/ctx_dep_mante_task/data.py
@@ -8,8 +8,8 @@ class CtxDepManteTask(Task):
                            coherences: list = None, scale_coherences: float = 0.1, scale_context: float = 0.2,
                            scale_output: float = 0.2, fixation_duration: float = 50, fixation_std: float = 0.2, 
                            input_start_time: float = 100, input_end_time: float = 400, initial_silence: float = 50,
-                           output_delay: float = 50, add_noise: bool = False, noise_scale: float = 0.02,
-                           *args, **kwargs):
+                           output_delay: float = 50, input_duration: float = 80, add_noise: bool = False,
+                           noise_scale: float = 0.03, *args, **kwargs):
 
         self.seed = seed
         np.random.seed(self.seed)
@@ -18,7 +18,6 @@ class CtxDepManteTask(Task):
             self.coherences = [-4, -2, -1, 1, 2, 4]
         else:
             self.coherences = coherences
-            
             
         #Assigning class variables
         self.root = root
@@ -39,10 +38,11 @@ class CtxDepManteTask(Task):
         self.fixation_duration = fixation_duration
         self.fixation_std = fixation_std
         self.scale_output = scale_output
+        self.input_duration = input_duration
         
         
         #Available versions can be seen here.
-        self.available_versions = {"vanilla"}
+        self.available_versions = {"vanilla","random_input"}
         
         """Input dimension is 4. This is because in this task, there are two stimuli, color and motion. And each corresponding can be activated in a given trial which constitutes third and fourth dimensions."""
         self.input_dims = 4
@@ -69,6 +69,7 @@ class CtxDepManteTask(Task):
         self.initial_silence_discrete = int(self.initial_silence // self.delta_t)
         self.output_delay_discrete = int(self.output_delay // self.delta_t)
         self.fixation_duration_discrete = int(self.fixation_duration // self.delta_t)
+        self.input_duration_discrete = int(self.input_duration // self.delta_t)
         
     def _check_input_validity(self):
         # Make sure the input is correct
@@ -82,6 +83,11 @@ class CtxDepManteTask(Task):
         assert (self.initial_silence > 0 and self.output_delay > 0), "input_duration and output_delay must be non-negative and greater than 0."
         assert (self.output_delay < self.duration - self.input_end_time), "Output delay time must be in the window of duration - input_end_time."
         assert all(isinstance(x, (int, float)) for x in self.coherences), f"Coherence list contains non-numeric elements! Given: {self.coherences}"
+        assert (self.input_duration >= 0 and self.input_duration < self.input_end_time - (self.input_start_time + self.initial_silence)), f"Input duration must be chosen such that it is suitable for input generation. It must be non-negative and smaller than {self.input_end_time - (self.input_start_time + self.initial_silence)}. However, given {self.input_duration}"
+        assert (self.input_duration >= self.delta_t), "Input duration must be greater than delta_t value."
+        
+        if (self.version == "random_input"):
+            assert (self.input_duration >= self.fixation_duration), f"Input duration must be greater or equal to fixatation duration. Given Input Duration: {self.input_duration} and Given Fixation Duration: {self.fixation_duration}"
         
     def __getitem__(self, index: int):
         return self.data[index]
@@ -90,6 +96,23 @@ class CtxDepManteTask(Task):
         inputs = np.zeros((self.T, self.input_dims))
         outputs = np.zeros((self.T, self.output_dims))
         
+        
+        
+        if self.version == "vanilla":
+            inputs, outputs, ctx_decision, input_start = self.gen_vanilla_input_output(inputs, outputs)
+        elif self.version == "random_input":
+            inputs, outputs, ctx_decision, input_start = self.gen_random_input_output(inputs, outputs)
+            
+           
+        
+        if self.add_noise:
+            noise = np.random.normal(0, self.noise_scale, self.T)
+            inputs[:, 0] +=noise #Adding noise to the motion coherence
+            inputs[:, 1] +=noise #Adding noise to the color coherence
+            
+        return inputs, outputs, ctx_decision, input_start
+    
+    def gen_vanilla_input_output(self, inputs, outputs):
         
         #Context decision is made.
         ctx_decision = np.random.choice([1, 2])
@@ -120,26 +143,67 @@ class CtxDepManteTask(Task):
             inputs[input_start + self.fixation_duration_discrete:self.input_end_time_discrete, 3] = 1 * self.scale_context
             outputs[self.input_end_time_discrete + self.output_delay_discrete: ,0] = 1*self.scale_output if (color_coh * self.scale_context) > 0 else -1*self.scale_output
             
+        return inputs, outputs, ctx_decision, input_start
+    
+    def gen_random_input_output(self, inputs, outputs):
         
-        if self.add_noise:
-            noise = np.random.normal(0, self.noise_scale, self.T)
-            inputs[:, 0] +=noise #Adding noise to the motion coherence
-            inputs[:, 1] +=noise #Adding noise to the color coherence
+        #Context decision is made.
+        ctx_decision = np.random.choice([1, 2])
+        
+        #Motion Coherence selection
+        if self.motion_coh == None:#if none, chose randomly
+            motion_coh = self.coherences[np.random.randint(0, len(self.coherences)-1)]
+        else:
+            motion_coh = self.motion_coh
             
-        return inputs, outputs
+        #Color Coherence  selection
+        if self.color_coh == None:#if none, chose randomly
+            color_coh = self.coherences[np.random.randint(0, len(self.coherences)-1)]
+        else:
+            color_coh = self.color_coh
+            
+        possible_input_start_point = self.input_start_time_discrete + self.initial_silence_discrete
+        possible_input_end_point = self.input_end_time_discrete - self.input_duration_discrete
+
+        locs = np.arange(possible_input_start_point, possible_input_end_point+1)
+        
+        input_start = np.random.choice(locs)
+        
+
+        
+        inputs[input_start:input_start + self.input_duration_discrete, 0] = motion_coh * self.scale_coherences #Motion coherence in dimension 0.
+        inputs[input_start:input_start + self.input_duration_discrete, 1] = color_coh * self.scale_context #Color coherence in dimension 1.
+
+        
+        #Context is selected.
+        if ctx_decision == 1:# Motion coherence is selected as the decision.
+            inputs[input_start + self.fixation_duration_discrete:self.input_end_time_discrete, 2] = 1 * self.scale_context
+            outputs[self.input_end_time_discrete + self.output_delay_discrete: ,0] = 1*self.scale_output if (motion_coh * self.scale_context) > 0 else -1*self.scale_output
+           
+        else: #ctx_decision == 2. Color coherence is selected as the decision.
+            inputs[input_start + self.fixation_duration_discrete:self.input_end_time_discrete, 3] = 1 * self.scale_context
+            outputs[self.input_end_time_discrete + self.output_delay_discrete: ,0] = 1*self.scale_output if (color_coh * self.scale_context) > 0 else -1*self.scale_output
+            
+        return inputs, outputs, ctx_decision, input_start
+        
         
     
     def gen_batch(self, batch_size):
         inputs = np.zeros((batch_size, self.T, self.input_dims))
         outputs = np.zeros((batch_size, self.T, self.output_dims))
- 
+        ctx_decision_list = []
+        input_start_list = []
+        
         for batch in range(batch_size):
-            inputs[batch], outputs[batch] = self.gen_input_output()
-        return inputs, outputs
+            inputs[batch], outputs[batch], ctx_decision, input_start = self.gen_input_output()
+            ctx_decision_list.append(ctx_decision)
+            input_start_list.append(input_start)
+            
+        return inputs, outputs, ctx_decision_list, input_start_list
     
     
-    def visualize_task(self, figsize=(20, 15), show_legend: bool = True):
-        inputs, outputs = self.gen_input_output()
+    def visualize_task(self, figsize=(16, 18), show_legend: bool = True):
+        inputs, outputs, ctx_decisions, input_start = self.gen_input_output()
         
         time = np.arange(0, self.duration, self.delta_t)
 
@@ -147,17 +211,77 @@ class CtxDepManteTask(Task):
         input_start_time = self.input_start_time
         input_end_time = self.input_end_time
         
-        fig, axs = plt.subplots(5, 1, figsize=figsize, sharex=True)
+        fig, axs = plt.subplots(4, 1, figsize=figsize, sharex=True)
         
         # Plotting Motion
-        axs[0].plot(time, inputs[:, 0], label="Input 1", color='r')
+        axs[0].plot(time, inputs[:, 0], label="Motion Context", color='r',lw=3)
         axs[0].set_title('Motion Coherence')
         axs[0].set_ylabel('Coherence Strength')
         axs[0].axvline(input_start_time, color='m', linestyle='--', label='Input Start/End Time')
         axs[0].axvline(input_end_time, color='m', linestyle='--')
+        # axs[0].axvspan(input_start_time + self.initial_silence,
+        #                input_start_time + self.initial_silence + self.fixation_duration,
+        #                color='green', alpha=0.3, label="Fixation period")
+        axs[0].axvspan(input_start*self.delta_t,
+                       input_start*self.delta_t + self.fixation_duration,
+                       color='green', alpha=0.3, label="Fixation period")
         if show_legend:
             axs[0].legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize='small')
-        axs[0].grid(True)
+        axs[0].grid(False)
+        
+        # Plotting Color Coherence
+        axs[1].plot(time, inputs[:, 1], label='Color Context', color='b',lw=3)
+        axs[1].set_title('Color Coherence')
+        axs[1].set_ylabel('Coherence Strength')
+        axs[1].axvline(input_start_time, color='m', linestyle='--', label='Input Start/End Time')
+        axs[1].axvline(input_end_time, color='m', linestyle='--')
+        # axs[1].axvspan(input_start_time + self.initial_silence,
+        #                input_start_time + self.initial_silence + self.fixation_duration,
+        #                color='green', alpha=0.3, label="Fixation period")
+        axs[1].axvspan(input_start*self.delta_t,
+                       input_start*self.delta_t + self.fixation_duration,
+                       color='green', alpha=0.3, label="Fixation period")
+        if show_legend:
+            axs[1].legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize='small')
+        axs[1].grid(False)
+        
+        # Plotting Decision on axsis 2 (motion)
+        axs[2].plot(time, inputs[:, 2], label='Decision on Motion', color="red",lw=3)
+        axs[2].plot(time, inputs[:, 3], label='Decision on Color', color="blue",lw=3)
+        axs[2].set_title('Context (Motion: Red Line | Color: Blue Line)')
+        axs[2].set_ylabel('Context Strength')
+        axs[2].axvline(input_start_time, color='m', linestyle='--', label='Input Start/End Time')
+        axs[2].axvline(input_end_time, color='m', linestyle='--')
+        if show_legend:
+            axs[2].legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize='small')
+        axs[2].grid(False)
+        
+        # Plotting Output (actual decision made)
+        if ctx_decisions == 1:#Motion was selected
+            decision_color = "red"
+            decision_label = "Motion is selected as decision. (Red Color, first axsis)"
+        else:#Color was selected
+            decision_color = "blue"
+            decision_label = "Color is selected as decision. (Blue Color, second axsis)"
+              
+        axs[3].plot(time, outputs[:, 0], label=decision_label, color=decision_color,lw=3)
+        axs[3].set_title('Decision Made')
+        axs[3].set_ylabel('Decision Strength')
+        axs[3].axvline(self.output_delay+self.input_end_time , color='k', linestyle='--', label='Output Start Time (Input End + Output Delay)')
+        if show_legend:
+            axs[3].legend(loc='lower left', bbox_to_anchor=(0, 0), fontsize='small')
+        axs[3].grid(False)
+        
+        # Ensure value 0 is fixed as the center on all subplots
+        for ax in axs:
+            ymin, ymax = ax.get_ylim()
+            limit = max(abs(ymin), abs(ymax))
+            ax.set_ylim(-limit, limit)
+            ax.axhline(0, color='black', linewidth=0.3)
+                
+        plt.suptitle(f"Context Dependent Mante Task | Version: {self.version}", fontsize=16)      
+        plt.savefig("sample.pdf")
+        plt.tight_layout()
             
     
     def visualize_rnn_output(self, model, loss=None, label_loss=None):
@@ -168,7 +292,7 @@ class CtxDepManteTask(Task):
 if __name__ == '__main__':
     np.random.seed(None)
     seed = np.random.randint(0,100)
-    task = CtxDepManteTask(seed=seed, version = "vanilla",add_noise = False)
+    task = CtxDepManteTask(seed=seed, version = "random_input",add_noise = False)
     task.visualize_task()
         
         
