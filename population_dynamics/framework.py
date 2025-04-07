21c44b4b5174c95f45c401abf36dd9791025335a
Alperen Cimen <alperen.cimen@ozu.edu.tr>
2025-04-07 10:45:26 +0300

    new modules are initiated

---
+++ population_dynamics/framework.py
@@ -0,0 +1,19 @@
+#Importing required libraries.
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from tasks.ctx_dep_mante_task.data import CtxDepManteTask
+
+
+
+#%%
+"""Importing mante task. If needed, the task parameters can be changed
+    by passing intented variable values in the function below."""
+    
+inputs, outputs, ctx_decisions,_,_ = CtxDepManteTask() 
+#%%
+
+"""Importing model weights"""
+
+#%%
+
